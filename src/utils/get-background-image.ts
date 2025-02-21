827c8500d901b2c6c2bd90d1708f714de1bdee46
Mateusz <mateusz.grzymowicz@gmail.com>
2025-02-21 11:00:14 +0100

    feat: add theme store and util for handling background images

---
+++ src/utils/get-background-image.ts
@@ -0,0 +1,22 @@
+import { ThemeEnum } from "../enums";
+
+const backgroundImages = {
+  grayscale: {
+    login: require("@/assets/app-images/grayscale/background-login.jpg"),
+    signup: require("@/assets/app-images/grayscale/background-signup.jpg"),
+    chat: require("@/assets/app-images/grayscale/background-chat.jpg"),
+    chat_dashboard: require("@/assets/app-images/grayscale/background-chat-dashboard.jpg"),
+    settings: require("@/assets/app-images/grayscale/background-settings.jpg"),
+  },
+  colorful: {
+    login: require("@/assets/app-images/colorful/background-login.jpg"),
+    signup: require("@/assets/app-images/colorful/background-signup.jpg"),
+    chat: require("@/assets/app-images/colorful/background-chat.jpg"),
+    chat_dashboard: require("@/assets/app-images/colorful/background-chat-dashboard.jpg"),
+    settings: require("@/assets/app-images/colorful/background-settings.jpg"),
+  },
+};
+
+export function getBackgroundImage(theme: ThemeEnum, page: keyof typeof backgroundImages.grayscale) {
+  return theme === ThemeEnum.GRAYSCALE ? backgroundImages.grayscale[page] : backgroundImages.colorful[page];
+}
