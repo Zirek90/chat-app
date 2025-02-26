d4d6ef88caeedefff02903229e7a822417a17150
Mateusz <mateusz.grzymowicz@gmail.com>
2025-02-26 15:39:59 +0100

    feat: add emoji modal

--- src/features/chat/message-form/MessageForm.tsx
+++ src/features/chat/message-form/MessageForm.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
-import { Ionicons } from '@expo/vector-icons';
+import React, { useEffect, useState } from 'react';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import EmojiPicker from 'rn-emoji-keyboard';
 import { View, TouchableOpacity, StyleSheet } from 'react-native';
 import { MessageFormInterface } from '../interfaces';
 import { TextInput } from '@/src/components';
@@ -8,6 +9,8 @@ import { COLORS } from '@/src/constants';
 export function MessageForm(props: MessageFormInterface) {
   const { onSend, editingMessage, onEditCancel } = props;
   const [input, setInput] = useState('');
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState(0);
 
   useEffect(() => {
     setInput(editingMessage?.content || '');
@@ -24,8 +27,21 @@ export function MessageForm(props: MessageFormInterface) {
     setInput('');
   }
 
+  function handleEmojiSelect(emoji: string) {
+    const beforeCursor = input.slice(0, cursorPosition);
+    const afterCursor = input.slice(cursorPosition);
+    const newText = beforeCursor + emoji + afterCursor;
+
+    setInput(newText);
+    setCursorPosition(cursorPosition + emoji.length);
+  }
+
   return (
     <View style={styles.container}>
+      <TouchableOpacity style={styles.iconButton}>
+        <MaterialIcons name="attach-file" size={24} color={COLORS.black} />
+      </TouchableOpacity>
+
       <TextInput
         style={styles.input}
         value={input}
@@ -34,6 +50,14 @@ export function MessageForm(props: MessageFormInterface) {
         multiline
         autoCorrect={false}
       />
+
+      <TouchableOpacity
+        onPress={() => setShowEmojiPicker(!showEmojiPicker)}
+        style={styles.iconButton}
+      >
+        <Ionicons name="happy-outline" size={24} color={COLORS.black} />
+      </TouchableOpacity>
+
       {editingMessage && (
         <TouchableOpacity onPress={onEditCancel} style={styles.cancelButton}>
           <Ionicons name="close" size={24} color={COLORS.white} />
@@ -42,6 +66,11 @@ export function MessageForm(props: MessageFormInterface) {
       <TouchableOpacity onPress={handleSend} style={styles.sendButton}>
         <Ionicons name="send" size={24} color={COLORS.white} />
       </TouchableOpacity>
+      <EmojiPicker
+        onEmojiSelected={(emojiObject) => handleEmojiSelect(emojiObject.emoji)}
+        open={showEmojiPicker}
+        onClose={() => setShowEmojiPicker(false)}
+      />
     </View>
   );
 }
@@ -74,5 +103,9 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.chatButton,
     borderRadius: 5,
     padding: 5,
+  },
+  iconButton: {
+    padding: 5,
+    marginRight: 5,
   },
 });
